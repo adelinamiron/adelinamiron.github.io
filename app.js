1eba19c6afc82191964f9dbeee84697e93866ce7
Adelina Miron <adelinamiron@Adelinas-MacBook-Pro.local>
2025-01-20 15:03:49 +0200

    Homepage updates

--- app.js
+++ app.js
@@ -25,4 +25,19 @@ $(document).ready(function () {
       $("body").removeClass("no-scroll");
     }
   });
+
+  // Video play on hover
+
+  $('.location-item').hover(
+    function () {
+      // Find the video inside the hovered location-item and play it
+      $(this).find('video')[0].play();
+    },
+    function () {
+      // Find the video inside the hovered location-item, pause and reset it
+      const video = $(this).find('video')[0];
+      video.pause();
+      video.currentTime = 0;
+    }
+  );
 });

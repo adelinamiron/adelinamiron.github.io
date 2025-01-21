35f22af3f03d3ae66612be9ef4630311c83ca8cf
Adelina Miron <adelinamiron@Adelinas-MacBook-Pro.local>
2025-01-21 19:59:31 +0200

    More ui updates

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
   $('.location-item').hover(
     function () {
       // Find the video inside the hovered location-item and play it
-      $(this).find('video')[0].play();
+      $(this).find('.video-desktop')[0].play();
     },
     function () {
       // Find the video inside the hovered location-item, pause and reset it
-      const video = $(this).find('video')[0];
+      const video = $(this).find('.video-desktop')[0];
       video.pause();
       video.currentTime = 0;
     }

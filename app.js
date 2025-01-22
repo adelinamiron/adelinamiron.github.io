a5fb63777a09a5835d35c4dc1c782c0b8ff00e46
Adelina Miron <adelinamiron@Adelinas-MacBook-Pro.local>
2025-01-23 00:07:07 +0200

    updates

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 $(document).ready(function () {
-
   // Video play on hover
-
-  $('.location-item').hover(
+  $(".video-hover").hover(
     function () {
-      // Find the video inside the hovered location-item and play it
-      $(this).find('.video-desktop')[0].play();
+      $(this).find(".video-desktop")[0].play();
     },
     function () {
-      // Find the video inside the hovered location-item, pause and reset it
-      const video = $(this).find('.video-desktop')[0];
+      const video = $(this).find(".video-desktop")[0];
       video.pause();
       video.currentTime = 0;
     }
   );
-  
 });
